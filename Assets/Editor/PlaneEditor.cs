3025eb9c1d9f7607f25b258d14876a72a7576bff
DESKTOP-QVTFV3S\o1834 <b02209046@ntu.edu.tw>
2018-06-12 15:05:08 +0800

    commit lost files

---
+++ Assets/Editor/PlaneEditor.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Plane))]
+
+public class PlaneEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Plane myScript = (Plane)target;
+        if (GUILayout.Button("Build Plane"))
+        {
+            myScript.BuildPlane();
+        }
+    }
+}
+#endif
